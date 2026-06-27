9a1e4fb092d44002f7b7f0c23ca0d5c700b77249
Iman Athirah <imanathirah14@gmail.com>
2026-06-27 09:57:59 +0800

    Day 4 Exercise 1

---
+++ js/javascript-demo.js
@@ -0,0 +1,15 @@
+const course ={
+    courseId: "C001",
+    title: "JavaScript Fundamentals",
+    durationHours: 12,
+    level: "Beginner",
+    instructor: "John Doe"
+
+}
+
+console.log("==== Course Details ==");
+console.log("Course ID: " + course.courseId);
+console.log("Title: " + course.title);
+console.log("Duration (Hours): " + course.durationHours);
+console.log("Level: " + course.level);
+console.log("Instructor: " + course.instructor);
